e7f1bbd93dec19b789dfe7c38fb55fbfc840ea55
mblee65 <mblee65@hanmail.net>
2020-03-12 13:37:00 +0900

    change

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,7 +81,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
@@ -99,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
